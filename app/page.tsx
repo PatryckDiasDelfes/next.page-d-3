c029217144a9016078d636b90e88eac96d19f6e0
Patryck Dias Delfes <delfesne@gmail.com>
2023-10-29 21:24:03 -0300

    add ex de button

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Article from "./components/article"
 import Footer from "./components/footer"
 import Header from "./components/header"
 import PostList from "./components/postList"
-import Sidebar from "./components/sidebar"
+// import Sidebar from "./components/sidebar"
 
 
 export default function Page() {
@@ -22,3 +22,28 @@ export default function Page() {
 
   )
 }
+
+
+buttonv   
+
+trap-button
+position: absolute;
+bottom: 88px;
+right: 76px;
+min-width: 125px;
+background: 8ECACC;
+color: white;
+border-radius: S5px;
+padding: 6.4rem 9.5rem;
+font-weight: 690;
+font-size: 18px;
+letter-spacing: 1px;
+text-transform: uppercase;
+)
+Okeyframes shaka (
+0% ( transform: scale(2.5)
+translateY(0%) rotate(-20deg); )
+100% ( transform: scale(2.5)
+translateY(80%) rotate(26deg); )
+)
+
